6cc45623dd2dba66c956fdf7b097b25f14fae911
Thomas Stekovits <t.stekovits@hotmail.com>
2019-03-01 08:42:08 +0100

    Add files via upload

---
+++ Lambda/Lambda/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Person(int id, string firstName, string lastName)
+        {
+            ID = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personList = new List<Person>();
+
+            personList.Add(new Person(1, "Tom", "Tomson"));
+            personList.Add(new Person(2, "Michael", "Johnson"));
+            personList.Add(new Person(3, "Tina", "Mayer"));
+            personList.Add(new Person(4, "Helene", "Klaus"));
+            personList.Add(new Person(5, "Paul", "Zach"));
+
+            string fName = "Tina";
+
+            Console.WriteLine($"Load Person with firstname \"{fName}\"\n");
+            Person p = personList.Where(x => x.FirstName == fName).FirstOrDefault();
+
+            if (p != null)
+            {
+                Console.WriteLine("Founded Person:");
+                Console.WriteLine($"ID: {p.ID}");
+                Console.WriteLine($"Firstname: {p.FirstName}");
+                Console.WriteLine($"Lastname: {p.LastName}");
+            }
+            else
+            {
+                Console.WriteLine("Person is NULL!!!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
