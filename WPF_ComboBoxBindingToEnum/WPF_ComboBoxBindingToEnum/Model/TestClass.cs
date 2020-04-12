9301fb4381e3c7cd71b0c084245572455ed5a593
Thomas Stekovits <t.stekovits@hotmail.com>
2020-04-13 00:04:14 +0200

    WPF ComboBox binding using Enumeratior
    Example how to easily use binding on Enums.
    Source from: https://brianlagunas.com/a-better-way-to-data-bind-enums-in-wpf/

---
+++ WPF_ComboBoxBindingToEnum/WPF_ComboBoxBindingToEnum/Model/TestClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WPF_ComboBoxBindingToEnum.Model
+{
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
+    public enum Status
+    {
+        [Description("This is horrible")]
+        Horrible,
+        [Description("This is bad")]
+        Bad,
+        [Description("This is so so")]
+        SoSo,
+        [Description("This is good")]
+        Good,
+        [Description("This is better")]
+        Better,
+        [Description("This is best")]
+        Best
+    }
+
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
+    public enum CarBrand
+    {
+        [Description("4 Rings")]
+        Audi,
+        [Description("Big German")]
+        BMW,
+        [Description("Woertersee-Car")]
+        Opel,
+        [Description("Golf Series")]
+        VW
+    }
+    public class TestClass
+    {
+        public Status Status { get; set; }
+        public CarBrand CarBrand { get; set; }
+    }
+}
