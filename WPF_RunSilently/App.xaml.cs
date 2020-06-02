b61d8dfdaef743868aeb7cd4a44ef834f8e26633
Thomas Stekovits <t.stekovits@hotmail.com>
2020-06-02 17:19:03 +0200

    Start WPF-Application in silent Mode
    Example of how to start a WPF-Application without a Window.

---
+++ WPF_RunSilently/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF_RunSilently.Model;
+
+namespace WPF_RunSilently
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            /// ##################################################################################
+            /// Validate CommandLine Arguments
+            /// ##################################################################################
+            string[] cmdArgs = Environment.GetCommandLineArgs();
+
+            if (null != cmdArgs && cmdArgs.Length > 1)
+            {
+                /// ##################################################################################
+                /// Do here your silent stuff:
+                /// ##################################################################################
+
+                Logging.LogParameters(cmdArgs);
+                this.Shutdown();
+            }
+            else
+            {
+                /// ##################################################################################
+                /// Create new MainWndow and start it:
+                /// ##################################################################################
+
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+        }
+    }
+}
---
+++ WPF_RunSilently/Model/Logging.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WPF_RunSilently.Model
+{
+    public static class Logging
+    {
+        private static readonly string m_strLogFilePath = $@"{System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)}\log\WPF_RunSilently.log";
+
+        /// <summary>
+        /// Logging in case of Silent-Mode
+        /// </summary>
+        /// <param name="strParameters"></param>
+        public static void LogParameters(string[] strParameters)
+        {
+            if (VerifyOrCreateLogFile())
+            {
+                File.AppendAllText(m_strLogFilePath, $"LogParameters() => {String.Join(", ", strParameters)}\r\n");
+            }
+        }
+
+        /// <summary>
+        /// Logging in case of MainWindow-Mode
+        /// </summary>
+        public static void LogFromMainWindow()
+        {
+            if (VerifyOrCreateLogFile())
+            {
+                File.AppendAllText(m_strLogFilePath, "LogFromMainWindow() => Call from MainWindow!\r\n");
+            }
+        }
+
+        /// <summary>
+        /// Verify if Log-File exist, if Log-File does not exist create it!
+        /// </summary>
+        /// <returns>Success/Fail</returns>
+        private static bool VerifyOrCreateLogFile()
+        {
+            try
+            {
+                /// ##################################################################################
+                /// Check if file exist
+                /// ##################################################################################
+
+                if (!System.IO.File.Exists(m_strLogFilePath))
+                {
+                    /// ##################################################################################
+                    /// Check if directory of file exist, if not create the directory:
+                    /// ##################################################################################
+
+                    if (!System.IO.Directory.Exists(System.IO.Path.GetDirectoryName(m_strLogFilePath)))
+                    {
+                        Directory.CreateDirectory(System.IO.Path.GetDirectoryName(m_strLogFilePath));
+                    }
+
+                    /// ##################################################################################
+                    /// Create file:
+                    /// ##################################################################################
+
+                    System.IO.File.Create(m_strLogFilePath).Close();
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine($"VerifyOrCreateLogFile() => Something happens during verification/creation of file, or directory:\n\"{ex.Message}\"");
+                return false;
+            }
+        }
+    }
+}
