cde947c0130401ccc64e8e25c361741692551c54
Thomas Stekovits <t.stekovits@hotmail.com>
2020-06-09 16:25:55 +0200

    Example of how to create a single Executable
    Example of how to create a single Executable without using an external tool.
    
    Take a look into the SingleExe.csproj
    Take a look into the App.xaml.cs

---
+++ SingleExe/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DemoApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// This musst be added that the application knows which *.dll it needs -> Newtonsoft.Json
+        /// </summary>
+        public App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            var requiredDllName = $"{(new AssemblyName(args.Name).Name)}.dll";
+            var resource = currentAssembly.GetManifestResourceNames().Where(s => s.EndsWith(requiredDllName)).FirstOrDefault();
+
+            if (null != resource)
+            {
+                using (var stream = currentAssembly.GetManifestResourceStream(resource))
+                {
+                    if (null == stream)
+                    {
+                        return null;
+                    }
+
+                    var block = new byte[stream.Length];
+                    stream.Read(block, 0, block.Length);
+                    return Assembly.Load(block);
+                }
+            }
+
+            return null;
+        }
+    }
+}
