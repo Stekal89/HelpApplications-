4b10b619f58988ded3eb2ad45e04a335f35f4c64
Thomas Stekovits <t.stekovits@hotmail.com>
2020-06-10 17:04:53 +0200

    Example -> How to get the current Directory
    1) Get current Application-Directory, where the exe-file is located
    2) Get current Working-Directory

---
+++ GetCurrentDirectory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace GetCurrentDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strExeDir = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            string strWorkingDir = Directory.GetCurrentDirectory();
+
+            Console.WriteLine($"Application-Dir: \"{strExeDir}\"");
+            Console.WriteLine($"Working-Dir: \"{strWorkingDir}\"");
+
+            Console.WriteLine("\n\nContinue with any key...");
+            Console.ReadKey();
+        }
+    }
+}
