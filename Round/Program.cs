200e27a7da6e1a0cf7c0bfd4c408d1629a362d7a
Thomas Stekovits <t.stekovits@hotmail.com>
2020-07-10 09:28:36 +0200

    Example of "Commercial Round"
    Commercial Round

---
+++ Round/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Round
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string strInput;
+
+			do
+            {
+				Console.Write("\nPlease enter a number:");
+				strInput = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(strInput))
+                {
+					if (decimal.TryParse(strInput, out decimal nInputToRound))
+					{
+						decimal nResult = Math.Round(nInputToRound, MidpointRounding.AwayFromZero);
+
+                        Console.WriteLine($"Result: {nResult}");
+					}
+					else
+					{
+						Console.WriteLine($"\nInput was not a number: \"{strInput}\"");
+					}
+				}
+                else
+                {
+                    Console.WriteLine($"\nInput is NULL or Empty!");
+                }
+
+                Console.WriteLine("\n\nContinue with any key...");
+				Console.ReadKey();
+				Console.Clear();
+
+			} while (strInput.ToUpper() != "E" && strInput.ToUpper() != "EXIT");
+        }
+	}
+}
